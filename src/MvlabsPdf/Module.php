b0917757d7390445b8b2509fa3d59790cab76f7a
Stefano Valle <s.valle@mvassociati.it>
2014-03-09 21:46:18 +0000

    Fix module.php

--- src/MvlabsPdf/Module.php
+++ src/MvlabsPdf/Module.php
@@ -7,4 +7,43 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-require_once __DIR__ . '/src/MvlabsPdf/Module.php';
+namespace MvlabsPdf;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+       
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+    
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'MvlabsPdf\Service\PdfService'    => '\MvlabsPdf\Service\PdfServiceFactory'
+            ),
+        );
+    }
+}
